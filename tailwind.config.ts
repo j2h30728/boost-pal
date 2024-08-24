254f3580cd1bff1702a621f0cebfcb64df4dd0a0
Dam <60846068+j2h30728@users.noreply.github.com>
2024-08-24 16:10:14 +0900

    feat: 기본 색상 토큰 및 기본 스타일 적용[#2]

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,27 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF5656",
+        "primary-1": "#FF6B6B",
+        "primary-2": "#FF8C8C",
+        secondary: "#F9D79F",
+        "secondary-1": "#9C8663",
+        border: "#E9E9E9",
+        "sub-border": "#F8F8F8",
+        neutral: "#4E4E4E",
+        base: "#636363",
+        underline: "#BABABA",
+        error: "#FF3737",
+      },
+      fontSize: {
+        md: "16px",
+      },
+      boxShadow: {
+        custom: "4.72px 6.3px 22.03px 0px #00000040",
+      },
+    },
   },
   plugins: [],
 };

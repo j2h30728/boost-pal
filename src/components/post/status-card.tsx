960cfa3dcc0a74059eeabfc37babdd9a1ece062b
Dam <60846068+j2h30728@users.noreply.github.com>
2024-08-26 04:36:23 +0900

    feat: 프로필 조회 페이지[#14]

--- src/components/post/status-card.tsx
+++ src/components/post/status-card.tsx
@@ -1,13 +1,17 @@
-export default function StatusCard() {
+import { getPostCountByLoggedInUser, getPostCountForThisMonth } from "@/service/postService";
+
+export default async function StatusCard() {
+  const postAllCount = await getPostCountByLoggedInUser();
+  const postCountForThisMonth = await getPostCountForThisMonth();
   return (
     <div className="flex justify-around">
       <div className=" *:font-semibold flex flex-col gap-1 items-center ">
         <span>누적 인증</span>
-        <div className="chip w-[60px]">10</div>
+        <div className="chip w-[60px]">{postAllCount}</div>
       </div>
       <div className=" *:font-semibold flex flex-col gap-1 items-center ">
         <span>이번달 인증</span>
-        <div className="chip w-[60px]">10</div>
+        <div className="chip w-[60px]">{postCountForThisMonth}</div>
       </div>
     </div>
   );

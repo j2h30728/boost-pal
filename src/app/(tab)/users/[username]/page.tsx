50acb3ec20d72bc2aabe242f3d540c66d35be4ed
Dam <60846068+j2h30728@users.noreply.github.com>
2024-08-26 05:58:43 +0900

    feat: 프로필 수정 [#14]

--- src/app/(tab)/users/[username]/page.tsx
+++ src/app/(tab)/users/[username]/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 import PostList from "@/components/post/post-list";
 import StatusCard from "@/components/post/status-card";
-import UserImage from "@/components/user/user-image";
 import { getUserInfoBySession } from "@/service/userService";
 import { getPostsByLoggedInUser } from "@/service/postService";
+import UserDefaultImage from "@/components/user/user-default-image";
 
 export default async function ProfilePage({ params }: { params: { username: string } }) {
   const loggedInUser = await getUserInfoBySession();
@@ -13,9 +13,12 @@ export default async function ProfilePage({ params }: { params: { username: stri
     <main className="flex flex-col gap-5">
       <div className="flex flex-col">
         <div className="p-5 flex items-center gap-3 ">
-          <UserImage />
+          <div className="flex gap-5 items-center">
+            <UserDefaultImage style="size-48" username={loggedInUser.username} avatar={loggedInUser.avatar} />
+            <span className="text-sm">{loggedInUser.username}</span>
+          </div>
           {params.username === loggedInUser.username && (
-            <Link className="ml-auto primary-button w-fit px-3 py-2" href={`edit`}>
+            <Link className="ml-auto primary-button w-fit px-3 py-2" href={`/users/${loggedInUser.username}/edit`}>
               내 정보 수정
             </Link>
           )}
--- src/components/user/profile-edit-form.tsx
+++ src/components/user/profile-edit-form.tsx
@@ -1,57 +1,128 @@
 "use client";
 
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import Button from "../common/button";
 import Input from "../common/input";
+import useUploadImage from "@/hooks/useUploadImage";
+import { profileSchema, ProfileType } from "@/lib/schema";
+import { InitialProfileType } from "@/service/userService";
+import { editProfile } from "@/app/(tab)/users/[username]/edit/actions";
 
-export default function ProfileEditForm() {
-  const [previewImage, setPreviewImage] = useState("");
-  const onImageChange = () => {};
+export default function ProfileEditForm({ initialUserInformation }: { initialUserInformation: InitialProfileType }) {
+  const { handleImageChange, setPreviewImage, state } = useUploadImage();
 
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    setError,
+    getValues,
+    formState: { errors },
+  } = useForm<ProfileType>({
+    resolver: zodResolver(profileSchema),
+    defaultValues: {
+      email: initialUserInformation.email ?? "",
+      username: initialUserInformation.username,
+      bio: initialUserInformation.bio ?? "",
+      photo: initialUserInformation.avatar ?? "",
+    },
+  });
+
+  const onSubmit = handleSubmit(async (data: ProfileType) => {
+    if (!state.imageFile) return alert("파일이 없다!");
+    const cloudflareForm = new FormData();
+    cloudflareForm.append("file", state.imageFile);
+    const response = await fetch(state.uploadUrl, {
+      method: "post",
+      body: cloudflareForm,
+    });
+    if (response.status !== 200) return;
+
+    const formData = new FormData();
+    formData.append("email", data.email);
+    formData.append("username", data.username);
+    formData.append("password", data.password);
+    formData.append("newPassword", data.newPassword ?? "");
+    formData.append("bio", data.bio ?? "");
+    formData.append("photo", data.photo ?? "");
+    const errors = await editProfile(formData);
+    setError("password", { message: errors?.fieldErrors.password?.at(0) });
+  });
+
+  const onValid = async () => {
+    await onSubmit();
+  };
+  const onClickCancelAvatar = () => {
+    setValue("photo", "");
+    setPreviewImage("");
+  };
   return (
-    <form className="flex flex-col gap-4">
+    <form action={onValid} className="flex flex-col gap-4">
       <div className="flex flex-col items-center">
-        <div className="size-20 bg-underline rounded-full"></div>
-        <label
-          htmlFor="photo"
-          style={{ backgroundImage: `url(${previewImage})` }}
-          className="primary-button w-20 text-xs rounded-3xl -mt-3">
-          {previewImage === "" ? <span>이미지 수정</span> : <span>취소</span>}
-        </label>
-        <input onChange={onImageChange} type="file" id="photo" name="photo" accept="image/*" className="hidden" />
+        <div
+          style={{ backgroundImage: `url(${state.previewImage ? state.previewImage : getValues("photo")})` }}
+          className={`size-20 rounded-full  bg-center bg-cover ${state.previewImage ? "" : "bg-underline"}`}></div>
+        <div className="flex -mt-3">
+          <label htmlFor="photo" className=" cursor-pointer primary-button w-20 text-xs rounded-3xl ">
+            <span>이미지 수정</span>
+          </label>
+          <button
+            className=" w-20 text-xs rounded-3xl  bg-secondary font-semibold text-neutral"
+            type="button"
+            onClick={onClickCancelAvatar}>
+            이미지 제거
+          </button>
+        </div>
+        <input
+          onChange={(e) => handleImageChange(e, setValue)}
+          type="file"
+          id="photo"
+          name="photo"
+          accept="image/*"
+          className="hidden"
+        />
       </div>
-      <Input label="이메일" type="email" required placeholder="이메일을 입력해주세요." name="email" errorMessage={""} />
+      <Input
+        label="이름"
+        type="text"
+        required
+        placeholder="이름을 입력해주세요."
+        errorMessage={errors.username?.message}
+        {...register("username")}
+      />
+      <Input
+        label="이메일"
+        type="email"
+        required
+        placeholder="이메일을 입력해주세요."
+        errorMessage={errors.email?.message}
+        {...register("email")}
+      />
       <Input
         label="현재 비밀번호"
         type="password"
         required
         placeholder="현재 비밀번호를 입력해주세요."
-        name="password"
-        errorMessage={"errors.password?.message"}
+        errorMessage={errors.password?.message}
+        {...register("password")}
       />
       <Input
         label="새 비밀번호"
         type="password"
         required={false}
         placeholder="변경할 새로운 비밀번호를 입력해주세요."
-        name="newPassword"
-        errorMessage={"errors.newPassword?.message"}
-      />
-      <Input
-        label="닉네임"
-        type="text"
-        required
-        placeholder="닉네임을 입력해주세요."
-        name="username"
-        errorMessage={"errors.username?.message"}
+        errorMessage={errors.newPassword?.message}
+        {...register("newPassword")}
       />
       <Input
         label="자기소개"
         type="text"
         required={false}
         placeholder="자기소개를 입력해주세요."
-        name="bio"
-        errorMessage={"errors.email?.message"}
+        errorMessage={errors.email?.message}
+        {...register("bio")}
       />
       <Button text="수정 완료" />
     </form>

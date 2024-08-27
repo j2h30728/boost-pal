9d9fef98607f7c99e71ae7bf0cee45b95495356b
Dam <60846068+j2h30728@users.noreply.github.com>
2024-08-27 19:12:30 +0900

    feat: 디자인 이미지 추가[#18]

--- src/app/(tab)/page.tsx
+++ src/app/(tab)/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { ChevronRightIcon } from "@heroicons/react/24/solid";
 
 import PostList from "@/components/post/post-list";
@@ -12,8 +13,22 @@ export default async function Home() {
   const category = await getMostPopularCategory();
   const { items: initialInfinitePosts, cursorId: initialInfiniteCursorId } = await getInitialPosts();
   return (
-    <main className="flex flex-col gap-4">
-      <div className="w-full h-96 bg-underline"></div>
+    <main className="flex flex-col gap-4 ">
+      <div className="relative ">
+        <h2 className="absolute left-5 bottom-10 text-xl font-bold">
+          <span className="text-primary">응원</span>과 함께 오늘을 <span className="text-primary">기록</span>해보아요!
+        </h2>
+        <div className="w-full h-80 relative -z-10">
+          <Image
+            className="right-0 absolute"
+            width={350}
+            height={400}
+            src="/images/main-image.png"
+            alt="main-image"
+            priority
+          />
+        </div>
+      </div>
       <div className="flex gap-2 pl-5 overflow-y-scroll">
         {/* #TODO 카테고리당 post list 페이지 생성 후 수정 필요 */}
         {["전체", ...Object.values(CATEGORIES)].map((category) => (
@@ -23,8 +38,8 @@ export default async function Home() {
         ))}
       </div>
       <div className="px-5 flex flex-col gap-5 ">
-        <div className="flex flex-col gap-2">
-          <p className="font-semibold text-xs pl-2">제일 인증수가 많은 주제에 함께 하세요!</p>
+        <div className="flex flex-col gap-2 *:bg-white">
+          <p className="font-semibold text-xs pl-2 w-fit p-1 rounded-xl">제일 인증수가 많은 주제에 함께 하세요!</p>
           <div className="flex justify-between px-2">
             <h3 className="font-extrabold text-md pb-2">최근 다미가 응원한 {CATEGORIES[category]} 인증</h3>
             {/* #TODO 카테고리당 post list 페이지 생성 후 수정 필요 */}
--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
 
 import { ROUTE_PATHS } from "@/constants/routePath";
+import Image from "next/image";
+
+import mainImage from "../../../public/images/main-image.png";
 
 export default function LogInPage() {
   return (
     <main className="h-screen flex flex-col gap-5 px-5 items-center justify-around">
-      <div className="flex flex-col gap-7 items-center justify-between">
-        <div className="w-64 h-64 bg-underline"></div>
-        <h1 className=" font-black text-[44px] ">이름 이름</h1>
+      <div className="w-full flex flex-col items-center justify-between relative">
+        <div className="w-72 h-96 relative">
+          <Image priority src="/images/main-image.png" width={288} height={384} alt="main-image" />
+        </div>
+        <h1 className=" font-black flex gap-2 text-[44px] absolute -translate-y-1/2 top-1/2">
+          <p className="first-letter:text-primary">BOOST</p>
+          <p className="first-letter:text-primary">PAL</p>
+        </h1>
       </div>
       <div className="w-full flex flex-col gap-3  items-center">
         <Link className="w-full primary-button" href={ROUTE_PATHS.LOG_IN}>
           이메일로 로그인
         </Link>
-        <Link href={ROUTE_PATHS.CREATE_ACCOUNT} className="text-underline text-xs">
+        <Link href={ROUTE_PATHS.CREATE_ACCOUNT} className="text-base text-sm">
           회원가입
         </Link>
       </div>

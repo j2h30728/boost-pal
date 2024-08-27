eed089f2a393bb3860caf06b006d16c151af5599
Dam <60846068+j2h30728@users.noreply.github.com>
2024-08-27 19:12:30 +0900

    feat: 디자인 이미지 추가[#18]

--- src/app/(auth)/create-account/page.tsx
+++ src/app/(auth)/create-account/page.tsx
@@ -7,8 +7,11 @@ export default function CreateAccountPage() {
   return (
     <main className="flex flex-col gap-5">
       <CreateAccount />
-      <p className="text-underline">
-        이미 계정이 있나요? <Link href={ROUTE_PATHS.LOG_IN}>로그인</Link>
+      <p className="text-base">
+        이미 계정이 있나요?
+        <Link className="text-secondary-1 ml-3" href={ROUTE_PATHS.LOG_IN}>
+          로그인
+        </Link>
       </p>
     </main>
   );
--- src/app/(auth)/log-in/page.tsx
+++ src/app/(auth)/log-in/page.tsx
@@ -1,20 +1,30 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { ROUTE_PATHS } from "@/constants/routePath";
 import LogInForm from "@/components/user/log-in-form";
 
 export default function LogInPage() {
   return (
-    <main className="flex flex-col gap-5 justify-between">
-      <h1 className="text-2xl w-64">
-        귀여운 친구의 <span className="text-primary font-semibold">응원</span>과 함께
-        <span className="text-primary font-semibold">오늘을 기록</span>하고 싶다면?
-      </h1>
-      <div className=" self-end w-36 h-36 bg-gray-200"></div>
-      <LogInForm />
-      <p className="text-underline">
-        아직 계정이 없으신가요? <Link href={ROUTE_PATHS.CREATE_ACCOUNT}>회원가입</Link>
-      </p>
+    <main className="flex flex-col gap-5">
+      <div className="flex flex-col justify-center">
+        <h1 className="text-2xl w-64 sm:mt-20">
+          귀여운 친구의 <span className="text-primary font-semibold">응원</span>과 함께
+          <span className="text-primary font-semibold">오늘을 기록</span>하고 싶다면?
+        </h1>
+        <div className=" self-end w-48 h-52 relative overflow-clip">
+          <Image width={192} height={208} src="/images/sub-image.png" alt="sub-image" />
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <LogInForm />
+        <p className="text-underline">
+          아직 계정이 없으신가요?
+          <Link className="text-secondary-1 ml-3" href={ROUTE_PATHS.CREATE_ACCOUNT}>
+            회원가입
+          </Link>
+        </p>
+      </div>
     </main>
   );
 }

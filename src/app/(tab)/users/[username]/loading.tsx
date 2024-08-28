2896da4ad34749d8d042c4585ca0d368503fbff1
Dam <60846068+j2h30728@users.noreply.github.com>
2024-08-28 21:53:01 +0900

    feat: 사용자 프로필 페이지 loading 추가 [#25]

---
+++ src/app/(tab)/users/[username]/loading.tsx
@@ -0,0 +1,26 @@
+import PostListSkeletonUI from "@/components/skeleton-ui/post-list-skeleton";
+import StatusCardSkeletonUI from "@/components/skeleton-ui/status-card-skeleton";
+
+export default async function HomeLoading() {
+  return (
+    <main className="flex flex-col gap-5">
+      <div className="flex flex-col">
+        <div className="p-5 flex items-center gap-5">
+          <div className="size-[65px] skeleton rounded-full "></div>
+          <div className="w-20 h-5 skeleton"></div>
+        </div>
+        <div className="*:skeleton w-full">
+          <div className="px-8 w-full"></div>
+          <div className="px-8 w-3/5"></div>
+        </div>
+      </div>
+      <div className="flex justify-around w-full">
+        <StatusCardSkeletonUI />
+        <StatusCardSkeletonUI />
+      </div>
+      <div className="p-5 flex flex-col gap-5">
+        <PostListSkeletonUI />
+      </div>
+    </main>
+  );
+}

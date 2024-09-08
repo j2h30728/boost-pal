740615364e4e03c500a9a0172b88b5c592f5646a
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-09 03:52:09 +0900

    feat: category path 생성 및 연결[#44]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import PostList from "@/components/post/post-list";
 import getMostPopularCategoryPosts, { getInitialPosts } from "@/service/postService";
 import InfinitePostList from "@/components/post/infinite-post-list";
 import { getMostPopularCategory } from "@/service/categoryService";
-import { CATEGORIES } from "@/constants/cateogries";
+import { CATEGORIES, makeCategoryPath } from "@/constants/cateogries";
 import TabBar from "@/components/common/tab-bar";
 import { getUserInfoBySession } from "@/service/userService";
 
@@ -36,11 +36,13 @@ export default async function Home() {
         </div>
       </div>
       <div className="flex gap-2 pl-5 overflow-y-scroll">
-        {/* #TODO 카테고리당 post list 페이지 생성 후 수정 필요 */}
-        {["전체", ...Object.values(CATEGORIES)].map((category) => (
-          <div className="bg-primary text-white px-4 py-2 min-w-fit rounded-2xl text-md font-bold" key={category}>
+        {Object.entries(CATEGORIES).map(([key, category]) => (
+          <Link
+            href={makeCategoryPath(key)}
+            className="bg-primary text-white px-4 py-2 min-w-fit rounded-2xl text-md font-bold"
+            key={category}>
             {category}
-          </div>
+          </Link>
         ))}
       </div>
       <div className="px-5 flex flex-col gap-5 ">
@@ -51,8 +53,7 @@ export default async function Home() {
             </p>
             <div className="flex justify-between pr-2 *:bg-white *:rounded-xl">
               <h3 className="font-extrabold text-md mb-2 px-2">최근 다미가 응원한 {CATEGORIES[category]} 인증</h3>
-              {/* #TODO 카테고리당 post list 페이지 생성 후 수정 필요 */}
-              <Link href={`/posts`} className="text-base flex items-center text-xs px-2">
+              <Link href={makeCategoryPath(category)} className="text-base flex items-center text-xs px-2">
                 <span>더보기</span> <ChevronRightIcon className="w-4 h-3" />
               </Link>
             </div>
@@ -62,7 +63,7 @@ export default async function Home() {
         <div className="flex flex-col gap-4">
           <div className="flex justify-between px-2">
             <h3 className="font-extrabold text-md">최근 다미가 응원한 인증</h3>
-            <Link href={"/posts/exercise"} className="text-base flex items-center text-xs">
+            <Link href={"/posts"} className="text-base flex items-center text-xs">
               <span>더보기</span> <ChevronRightIcon className="w-4 h-3" />
             </Link>
           </div>
--- src/constants/cateogries.ts
+++ src/constants/cateogries.ts
@@ -1,4 +1,8 @@
+import { isCategory } from "@/lib/utils";
+import { ROUTE_PATHS } from "./routePath";
+
 export const CATEGORIES = {
+  ALL: "전체",
   EXERCISE: "운동",
   STUDY: "공부",
   DIET: "식단",
@@ -6,3 +10,10 @@ export const CATEGORIES = {
   READING: "독서",
   OTHER: "기타",
 } as const;
+
+export const makeCategoryPath = (category: string) => {
+  if (!isCategory(category)) {
+    return ROUTE_PATHS.POSTS;
+  }
+  return [ROUTE_PATHS.POSTS, "category", category.toLowerCase()].join("/");
+};

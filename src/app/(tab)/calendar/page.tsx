9a98a0a195a40868b9b57e61e57f9dcd28286891
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-18 20:59:26 +0900

    fix: force-dynamic 문구를 추가하여 동적라우팅 설정[#30]

--- src/app/(tab)/calendar/page.tsx
+++ src/app/(tab)/calendar/page.tsx
@@ -1,6 +1,8 @@
 import PostCalendar from "@/components/calendar/post-calendar";
 import StatusCard from "@/components/post/status-card";
 
+export const dynamic = "force-dynamic";
+
 export default function CalendarPage() {
   return (
     <div className="flex flex-col justify-center items-center">

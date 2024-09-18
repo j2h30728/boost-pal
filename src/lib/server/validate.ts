791f1864aed7572a220e496e097082689a04cf76
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-18 22:05:07 +0900

    fix: 에러 처리관련 사용자 정보 수정 에러 해결[#30]

--- src/lib/server/validate.ts
+++ src/lib/server/validate.ts
@@ -2,38 +2,51 @@
 
 import { getUserAuthInfo, getUserIdByEmail, getUserByUsername, getSessionId } from "@/service/userService";
 import bcrypt from "bcrypt";
+import { NotFoundError } from "../error/customError";
 
 export const isUsernameExists = async (username: string) => {
-  const user = await getUserByUsername(username);
-  return Boolean(user);
+  try {
+    const { data } = await getUserByUsername(username);
+    return Boolean(data);
+  } catch (error) {
+    return false;
+  }
 };
 
 export const isEmailExists = async (email: string) => {
-  const user = await getUserIdByEmail(email);
-
-  return Boolean(user);
+  try {
+    const { data } = await getUserIdByEmail(email);
+    return Boolean(data);
+  } catch (error) {
+    return false;
+  }
 };
 
 export const checkEmailAvailability = async (email: string) => {
-  const sessionId = await getSessionId();
-  const { data: user } = await getUserIdByEmail(email);
+  try {
+    const sessionId = await getSessionId();
+    const { data: user } = await getUserIdByEmail(email);
 
-  if (sessionId === user?.id) {
-    return !Boolean(user);
+    if (sessionId === user?.id) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    return false;
   }
-
-  return Boolean(user);
 };
 
 export const checkUsernameAvailability = async (username: string) => {
-  const sessionId = await getSessionId();
-  const { data: user } = await getUserByUsername(username);
-
-  if (sessionId === user?.id) {
-    return !Boolean(user);
+  try {
+    const sessionId = await getSessionId();
+    const { data: user } = await getUserByUsername(username);
+    if (sessionId === user?.id) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    return false;
   }
-
-  return Boolean(user);
 };
 
 export const checkUserPassword = async (password: string) => {

c3a9fe8ddec0400d6e5b32b99d9a95f8c802a3b4
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-18 11:47:52 +0900

    feat: 검색 컴포넌트 수정[#30]

--- src/components/post/search-post-list.tsx
+++ src/components/post/search-post-list.tsx
@@ -1,12 +1,18 @@
 import { ListOfPost } from "@/lib/types";
 import PostList from "./post-list";
 
-export default function SearchedPostList({ searchPosts }: { searchPosts?: ListOfPost[] | null }) {
+export default function SearchedPostList({
+  searchPosts,
+  message,
+}: {
+  searchPosts?: ListOfPost[] | null;
+  message?: string;
+}) {
   if (!searchPosts) {
     return <PostList initialPosts={[]} />;
   }
   if (searchPosts.length === 0) {
-    return <p>검색 결과가 존재하지 않습니다.</p>;
+    return <p>{message ?? "검색 결과가 존재하지 않습니다"}.</p>;
   }
   return <PostList initialPosts={searchPosts} />;
 }

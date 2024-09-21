9bc314d5b1dda289c62b0b60720c245cba66ef53
Dam <60846068+j2h30728@users.noreply.github.com>
2024-09-21 23:54:06 +0900

    fix: use server 문구 추가하여 서버에서 이미지 요청하게 에러 수정[#9]

--- src/service/imageService.ts
+++ src/service/imageService.ts
@@ -1,3 +1,5 @@
+"use server";
+
 export async function getUploadUrl() {
   const response = await fetch(
     `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`,

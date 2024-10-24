84f9fb60d4e44a4b806df6793eb85aa625b4ed65
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-24 16:52:28 +0900

    feat: 카카오 로그아웃 구현 [#20]

--- src/app/(tab)/users/[username]/actions.ts
+++ src/app/(tab)/users/[username]/actions.ts
@@ -1,10 +1,15 @@
 "use server";
 
+import { kakaoLogOut } from "@/app/(auth)/oauth/kakao/redirect/actions";
+import { ROUTE_PATHS } from "@/constants/routePath";
 import { getSession } from "@/lib/server/session";
 import { redirect } from "next/navigation";
 
 export const logOut = async (_: FormData) => {
   const session = await getSession();
+  if (session.oauth) {
+    await kakaoLogOut();
+  }
   session.destroy();
-  redirect("/intro");
+  redirect(ROUTE_PATHS.INTRO);
 };

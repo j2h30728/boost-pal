5ad12112db024bcb6c11a1c5f51da497921f6bd0
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-24 17:23:04 +0900

    feat: 인트로 페이지에서 카카오 로그인 버튼 스타일 적용[#20]

--- src/app/intro/page.tsx
+++ src/app/intro/page.tsx
@@ -5,6 +5,7 @@ import { ROUTE_PATHS } from "@/constants/routePath";
 import Logo from "@/components/common/Logo";
 
 import mainImage from "../../../public/images/main-image.webp";
+import { KaKaoIcon } from "@/components/common/incons";
 
 export default function IntroPage() {
   return (
@@ -19,13 +20,13 @@ export default function IntroPage() {
       </div>
       <div className="w-full flex flex-col gap-3  items-center">
         <Link className="w-full primary-button" href={ROUTE_PATHS.LOG_IN}>
-          이메일로 로그인
+          이메일 로그인
         </Link>
         <Link href={ROUTE_PATHS.CREATE_ACCOUNT} className="text-base text-sm">
           회원가입
         </Link>
-        <Link href={ROUTE_PATHS.KAKAO_LOG_IN} className="text-base text-sm">
-          카카오 로그인
+        <Link className="w-full flex gap-3 primary-button bg-yellow-300 text-neutral" href={ROUTE_PATHS.KAKAO_LOG_IN}>
+          <KaKaoIcon width={25} height={20} /> 카카오 로그인
         </Link>
       </div>
     </main>

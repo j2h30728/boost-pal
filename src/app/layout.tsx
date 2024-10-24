3a0cb364f4e2000cef29269f272150c11bcc2444
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-25 00:56:38 +0900

    feat: metadata 설정[#52]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,10 +6,39 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Boost Pal",
-  description: "",
+  title: "BOOST PAL - 부스트팔",
+  description: "부스트팔은 일상적인 기록을 저장하고 응원을 받을 수 있는 서비스입니다.",
+  keywords: ["BOOST PAL", "부스트팔", "일상 기록", "동기부여", "챌린지", "daily goals", "motivation", "challenge"],
+  metadataBase: new URL("https://boostpal.vercel.app"),
+  icons: { icon: "/favicons/favicon.svg", apple: "/favicons/apple-touch-icon.png" },
+  openGraph: {
+    title: "BOOST PAL - 부스트팔",
+    description: "부스트팔에서 일상을 기록하고 응원을 받으세요.",
+    url: "https://boostpal.vercel.app",
+    siteName: "BOOST PAL",
+    images: [
+      {
+        url: "/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "BOOST PAL OG Image",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@boostpal",
+    title: "BOOST PAL - 부스트팔",
+    description: "부스트팔에서 목표를 기록하고 응원을 받으세요.",
+    images: ["/images/og-image.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{

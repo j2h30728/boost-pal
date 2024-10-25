182fcac73962dff2f6eb16b582c1372000b20799
Dam <60846068+j2h30728@users.noreply.github.com>
2024-10-25 09:43:46 +0900

    feat: photo가 없다면 대체이미지사용 [#52]

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,5 +26,5 @@ export async function middleware(req: NextRequest) {
   }
 }
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicons).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicons|images).*)"],
 };

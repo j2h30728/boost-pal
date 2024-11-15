0a3a580d3f5b984f8eb098d0d2e5977f7fa802b0
Dam <60846068+j2h30728@users.noreply.github.com>
2024-11-15 17:47:12 +0900

    feat: cloudflare에 등록된 이미지를 서버에 등록할때 포맷팅해주는 함수 생성[#56]

---
+++ src/lib/images.ts
@@ -0,0 +1,12 @@
+export const IMAGES_OPTIONS = {
+  SMALL: "small",
+  MIDDLE: "middle",
+} as const;
+
+export const createUploadImageUrl = (
+  imageUrl?: string,
+  option?: (typeof IMAGES_OPTIONS)[keyof typeof IMAGES_OPTIONS]
+) => {
+  if (!imageUrl) return "";
+  return option ? `${imageUrl}/${option}` : imageUrl;
+};
